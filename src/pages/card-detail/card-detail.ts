6057260d5b5eed77f05a905250b386d287edd74f
tomwey <tomwey@163.com>
2017-11-23 15:10:07 +0800

    add some func

--- src/pages/card-detail/card-detail.ts
+++ src/pages/card-detail/card-detail.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ToolService } from '../../providers/tool-service';
+import { CardsService } from '../../providers/cards-service';
 
 /**
  * Generated class for the CardDetailPage page.
@@ -20,12 +21,29 @@ export class CardDetailPage {
     public navCtrl: NavController, 
     public navParams: NavParams,
     private toolService: ToolService,
+    private cards: CardsService,
   ) {
     this.card = this.navParams.data;
   }
 
   take() {
-    
+    this.toolService.showLoading('领取中...');
+    this.cards.takeCard(this.card)
+      .then(data => {
+        this.toolService.hideLoading();
+
+        this.card.taked = true;
+
+        setTimeout(() => {
+          this.toolService.showToast('领取成功');
+        }, 100);
+      })
+      .catch(error => {
+        this.toolService.hideLoading();
+        setTimeout(() => {
+          this.toolService.showToast(error);
+        }, 100);
+      });
   }
 
 }
--- src/providers/cards-service.ts
+++ src/providers/cards-service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ApiService } from './api-service';
 // import { Storage }    from '@ionic/storage';
 import { UserService } from './user-service';
+import { LocationService } from './location-service';
 // import { QQMaps } from './qq-maps';
 // import { LocationService } from './location-service';
 
@@ -11,7 +12,7 @@ export class CardsService {
   constructor(private api: ApiService,
               private user: UserService,
               // private qqMaps: QQMaps
-              // private locService: LocationService
+              private locService: LocationService
             ) {
     
   }
@@ -28,6 +29,33 @@ export class CardsService {
           .catch(error => reject(error));
       }).catch(error => reject(error));
     });
+  }
+
+  takeCard(card): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.user.token().then(token => {
+        this.locService.getUserPosition(true, true)
+          .then(pos => {
+            this._takeCard(card, pos, token)
+              .then(data => resolve(data))
+              .catch(error => reject(error));
+          })
+          .catch(error => {
+            this._takeCard(card, null, token)
+              .then(data => resolve(data))
+              .catch(error => reject(error));
+          });
+      })
+    });
+  }
+
+  private _takeCard(card, pos, token): Promise<any> {
+    // console.log(JSON.stringify(card));
+    let loc = null;
+    if (pos) {
+      loc = `${pos.lng},${pos.lat}`;
+    }
+    return this.api.post(`cards/${card.id}/take`, { token: token, loc: loc });
   }
 
   getUserCards(pageNo: number = 1, pageSize: number = 20): Promise<any> {
